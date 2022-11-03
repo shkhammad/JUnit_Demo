e53f639af1a33bdc7c85e5c09438042f56a61bcf
Hammad Shaikh <sikandarbakht59@gmail.com>
2022-11-03 16:14:22 +0530

    First Commit for Testing

---
+++ src/main/java/com/hammad/BasicTest.java
@@ -0,0 +1,9 @@
+package com.hammad;
+
+public class BasicTest {
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
+	
+}
---
+++ src/test/java/maven_demo/TestCase.java
@@ -0,0 +1,30 @@
+package maven_demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;  
+
+
+import com.hammad.BasicTest;
+
+public class TestCase {
+	
+	@Test
+	public void testAdd() {
+		assertEquals(5, BasicTest.add(2, 3));
+		//assertEquals(2, BasicTest.add(2, 0));
+		
+		//assertEquals("Hammad","Hamma");
+		
+		//assertTrue("Hammad", true);
+		//assertFalse("Hammad", false);
+		
+		
+		
+		//assertFalse("Hi",3>2);
+		
+	}
+	
+}
